46efe3f169bd217f7642ee6572973f4880c27eca
Hausk <hauskwar@gmail.com>
2023-07-17 13:13:38 +0200

    update.

--- resources/js/Components/Navbar.vue
+++ resources/js/Components/Navbar.vue
@@ -1,13 +1,13 @@
 <template>
-    <div as="nav" class="bg-gray-800">
+    <div as="nav" class="bg-transparent">
 		<div class="mx-auto px-2 md:px-6 lg:px-8">
             <div class="relative flex h-16 items-center justify-between">
         		<div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
 		            <!-- Mobile menu button-->
     		        <div  class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
         		        <span class="sr-only">Open main menu</span>
-            		    <font-awesome-icon v-on:click="open = !open" v-if="!open" :icon="['fas', 'bars']" />
-						<font-awesome-icon v-on:click="open = !open" v-if="open" :icon="['fas', 'xmark']" />
+            		    <font-awesome-icon v-on:click="openDrop" v-if="!open" :icon="['fas', 'bars']" />
+						<font-awesome-icon v-on:click="openDrop" v-if="open" :icon="['fas', 'xmark']" />
             		</div>
 		        </div>
         		<div class="flex flex-2 items-center justify-center md:items-stretch md:justify-start">
@@ -30,8 +30,8 @@
 					</div>
 			</div>
 		</div>
-  
-		<div v-if="open" class="lg:hidden h-screen">
+
+		<div class="lg:hidden h-0 bg-gradient" :class="{ isOpen : open }">
 			<div class="space-y-1 px-2 pb-3 pt-2">
 				<div v-for="item in navigation" :key="item.name" as="a" :href="item.href" class="m-auto text-center" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block rounded-md px-3 py-2 text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</div>
 				<div class="flex justify-between w-1/3 m-auto">
@@ -45,17 +45,27 @@
 		</div>
 	</div>
 </template>
-  
-
 <script setup>
 import { Link } from '@inertiajs/vue3';
 import { ref } from 'vue';
-
 const open = ref(false);
-
-  const navigation = [
+const openDrop = () => {
+    open.value = !open.value;
+};
+const navigation = [
     { name: 'HOME', href: '/' },
     { name: 'WORKS', href: '/works' },
-  ]
-
+]
 </script>
+<style>
+    .bg-gradient {
+        background: linear-gradient(125deg, #222, #202020);
+        transition: .2s linear;
+    }
+    .isOpen {
+        height: 100vh;
+    }
+    .isOpen div {
+        opacity: 1;
+    }
+</style>
